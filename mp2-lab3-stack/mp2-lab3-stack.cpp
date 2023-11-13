de6882492a28a412c1c653076c7fe9e42c2e1adb
Konstantin Barkalov <barkalov@vmk.unn.ru>
2023-11-13 16:41:27 +0300

    first version

---
+++ mp2-lab3-stack/mp2-lab3-stack.cpp
@@ -0,0 +1,13 @@
+// mp2-lab3-stack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+/*
+Написана реализация класса TStack
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
