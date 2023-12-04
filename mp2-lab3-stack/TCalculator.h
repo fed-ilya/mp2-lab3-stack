ce5e8405529cbb8ac747293e8586f265b69e3773
fed-ilya <fed.ilya2004@yandex.ru>
2023-12-04 05:21:51 +0300

    My first commit

--- mp2-lab3-stack/TCalculator.h
+++ mp2-lab3-stack/TCalculator.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <string>
+using namespace std;
+
 class TCalculator
 {
+	string infix, postfix;
+	int prior(char op);
 public:
 	TCalculator();
+
 };
 
+int prior(char op) {
+	
+}
--- mp2-lab3-stack/TStack.h
+++ mp2-lab3-stack/TStack.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 /*
 Реализация класса TStack
@@ -8,5 +10,97 @@ template <class T>
 class TStack
 {
 	T* pMem;
+	int MaxSize;
+	int CurrInd;
+public:
+	TStack(int _MaxSize = 10) {
+		if (_MaxSize <= 0) {
+			MaxSize = _MaxSize;
+			pMem = new T[MaxSize];
+			CurrInd = -1;
+		}
+		else throw "wrong Size";
+	};
+
+	~TStack() {
+		delete[] pMem;
+	};
+
+	TStack(const TStack& st) {
+		MaxSize = st.MaxSize;
+		CurrInd = st.CurrInd;
+		pMem = new T[MaxSize];
+		for (int i = 0; i <= MaxSize; i++) {
+			pMem[i] = st.pMem[i];
+		}
+	};
+
+	TStack& operator=(const TStack& st) {
+		if (this != &st) {
+			if (MaxSize != st.MaxSize) {
+				delete[] pMem;
+				MaxSize = st.MaxSize;
+				CurrInd = st.CurrInd;
+				pMem = new T[MaxSize];
+			}
+			for (int i = 0; i < MaxSize; i++) {
+				pMem[i] = st.pMem[i];
+			}
+			return *this;
+		}
+	};
+
+	void print() {
+		for (int i = 0; i < MaxSize; i++) {
+			cout << pMem[i];
+		}
+	};
+
+	bool empty() {
+		if (CurrInd == -1) return true;
+		else return false;
+	};
+
+	bool fool() {
+		if (CurrInd == MaxSize - 1) return true;
+		else return false;
+	};
+
+	void push(const T& el) {
+		if (CurrInd + 1 >= MaxSize) throw "Stack overflow";
+		pMem[CurrInd + 1] = el;
+		CurrInd++;
+	};
+
+	T pop() {
+		if (CurrInd == -1) throw "Stack is empty";
+		CurrInd--;
+		return pMem[CurrInd + 1];
+	};
+	
+	T top() { //"Просто посмотреть"
+		if (CurrInd == -1) throw "Stack is empty";
+		return pMem[CurrInd];
+	};
+
+	void clear() { CurrInd = -1; }
+
+	bool proverka_skob(string str) {
+		TStack<T> s(20);
+		int res = 0;
+		for (int i = 0; i < str.size(); i++) {
+			if (str[i] == '(') s.push(str[i]);
+			else {
+				if (str[i] == ')') {
+					if (!s.empty()) s.pop();
+					else res = 1;
+				}
+			}
+		}
+		if (!s.empty()) res = 2;
+		if (res == 0) return true;
+		else if (res == 1) throw "Лишняя ')'";
+		else throw "Лишняя '('";
+	}
 };
 
